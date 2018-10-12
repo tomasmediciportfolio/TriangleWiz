b67f6b09629ed369a51c5bf4e2949054c7fc72c1
tomasmedici <tomas@medici.dk>
2018-10-12 13:03:07 +0200

    This is first commit

---
+++ TriangleWiz/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TriangleWiz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is TriangleWiz for Danske Bank");
+        }
+    }
+}
